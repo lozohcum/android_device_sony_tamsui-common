14fa532a2e7dcede454f5f9205abbaf723a30c03
Adam Farden <adam@farden.cz>
2013-03-23 22:58:59 +0100

    remove broken support for FM Radio

--- tamsui.mk
+++ tamsui.mk
@@ -16,7 +16,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # EGL config
@@ -75,10 +74,6 @@ PRODUCT_PACKAGES += \
 # Misc
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
-
-# FmRadio
-#PRODUCT_PACKAGES += \
-#    FmRadioReceiver
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
